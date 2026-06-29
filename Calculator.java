eedd08e4ae423699fcdf5e7c123926a09f978720
kavyap09 <152002304+kavyap09@users.noreply.github.com>
2026-06-30 02:53:08 +0530

    Simplify output statements in Calculator
    Removed labels from output statements for cleaner display.

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,9 @@
 public class Calculator {
 public static void main(String[] args) {
 int a = 20, b = 4;
-System.out.println(&quot;ADD: &quot; + (a+b));
-System.out.println(&quot;SUB: &quot; + (a-b));
-System.out.println(&quot;MUL: &quot; + (a*b));
-System.out.println(&quot;DIV: &quot; + (a/b));
+System.out.println((a+b));
+System.out.println( (a-b));
+System.out.println( (a*b));
+System.out.println( (a/b));
 }
 }
